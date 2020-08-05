59807bf9ba6158e4a1a1eeb6ebfdfd2d7d8d0ae7
Hiroaki Otsu <ootsuhiroaki@gmail.com>
2020-08-05 23:08:57 +0900

    Bump up version

--- e2wm-perspb.el
+++ e2wm-perspb.el
@@ -5,7 +5,7 @@
 ;; Author: Hiroaki Otsu <ootsuhiroaki@gmail.com>
 ;; Keywords: tools, window manager
 ;; URL: https://github.com/aki2o/e2wm-perspb
-;; Version: 0.0.3
+;; Version: 0.0.4
 ;; Package-Requires: ((e2wm "1.2") (persp-mode "2.9.4") (dash "2.12.0") (deferred "0.3.1") (yaxception "0.3.2") (all-the-icons "3.2.0"))
 
 ;; This program is free software; you can redistribute it and/or modify
